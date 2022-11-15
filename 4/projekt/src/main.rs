12df906d06caf9b2c4c88a362ccae63659d794fd
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-15 17:24:17 +0100

    Image write to file method added.

--- 4/projekt/src/main.rs
+++ 4/projekt/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{Error, Write};
 
 struct Image {
     width: usize,
@@ -45,12 +47,32 @@ impl Image {
     fn get_pixel_color(&self, row: usize, column: usize) -> (u8, u8, u8) {
         self.pixels[row][column]
     }
+
+    fn save_to_file(&self, file_name: &str) -> Result<(), Error> {
+        let mut file = File::create(file_name)?;
+        writeln!(file, "P6")?;
+        writeln!(file, "{}", format!("{} {}", self.width, self.height))?;
+        writeln!(file, "255")?;
+
+        self.pixels
+            .iter()
+            .for_each(|row: &Vec<(u8, u8, u8)>| {
+                row
+                    .iter()
+                    .for_each(|pixel: &(u8, u8, u8)| {
+                        write!(file, "{}", format!("{} {} {} ", pixel.0, pixel.1, pixel.2));
+                    });
+                writeln!(file, "");
+            });
+
+        Ok(())
+    }
 }
 
 fn main() {
     let mut img = Image::new(2, 2, vec![vec![(1, 2, 3), (4, 5, 6)], vec![(7, 8, 9), (10, 11, 12)]]);
 
-    let img_white = Image::new_white(2, 2);
+    let img_white = Image::new_white(250, 250);
 
     img.print();
     //img_white.print();
@@ -60,4 +82,6 @@ fn main() {
     img.print();
 
     println!("pixel color: {:?}", img.get_pixel_color(0, 0));
+
+    img_white.save_to_file("test.ppm");
 }
