8fb383f905cce5a5748c81011db84549e7d51cbe
MarcinBieganek <marcin.bieganek@gmail.com>
2022-11-15 15:32:37 +0100

    Setter and getter for pixel rgb color added.

--- 4/projekt/src/main.rs
+++ 4/projekt/src/main.rs
@@ -1,27 +1,27 @@
 
 struct Image {
-    width: u32,
-    height: u32,
+    width: usize,
+    height: usize,
     pixels: Vec<Vec<(u8, u8, u8)>>
 }
 
 impl Image {
-    fn new_white(width: u32, height: u32) -> Image {
+    fn new_white(width: usize, height: usize) -> Image {
         Image {
             width: width,
             height: height,
-            pixels: vec![vec![(255, 255, 255); width as usize]; height as usize]
+            pixels: vec![vec![(255, 255, 255); width]; height]
         }
     }
 
-    fn new(width: u32, height: u32, pixels: Vec<Vec<(u8, u8, u8)>>) -> Image {
-        if pixels.len() != (height as usize) {
+    fn new(width: usize, height: usize, pixels: Vec<Vec<(u8, u8, u8)>>) -> Image {
+        if pixels.len() != height {
             panic!("Tried to create Image with wrong height input!");
         }
         pixels
             .iter()
             .for_each(|row: &Vec<(u8, u8, u8)>| {
-                if row.len() != (width as usize) {
+                if row.len() != width {
                     panic!("Tried to create Image with wrong width input!");
                 }
             });
@@ -37,13 +37,27 @@ impl Image {
         println!("IMG height: {}", self.height);
         println!("IMG content: {:?}", self.pixels);
     }
+
+    fn change_pixel_color(&mut self, row: usize, column: usize, rgb: (u8, u8, u8)) {
+        self.pixels[row][column] = rgb;
+    }
+
+    fn get_pixel_color(&self, row: usize, column: usize) -> (u8, u8, u8) {
+        self.pixels[row][column]
+    }
 }
 
 fn main() {
-    let img = Image::new(2, 2, vec![vec![(1, 2, 3), (4, 5, 6)], vec![(7, 8, 9), (10, 11, 12)]]);
+    let mut img = Image::new(2, 2, vec![vec![(1, 2, 3), (4, 5, 6)], vec![(7, 8, 9), (10, 11, 12)]]);
 
     let img_white = Image::new_white(2, 2);
 
     img.print();
-    img_white.print();
+    //img_white.print();
+
+    img.change_pixel_color(1, 0, (0, 0, 0));
+
+    img.print();
+
+    println!("pixel color: {:?}", img.get_pixel_color(0, 0));
 }
